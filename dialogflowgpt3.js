fc8fe4d54e22711801a194ad5b89bb62357d9d55
Md Khussairiee Bin Ahmad <111945064+mdkhussairiee@users.noreply.github.com>
2022-11-10 21:20:30 +0800

    Update dialogflowgpt3.js

--- dialogflowgpt3.js
+++ dialogflowgpt3.js
@@ -14,7 +14,7 @@ app.post("/dialogflow", express.json(), (req, res) => {
     agent.handleRequest(intentMap);
   
     function welcome(agent) {
-      agent.add('Hi, I am Sophea! I am your virtual personal assistant from Asrix Prime. How are you doing today?');
+      agent.add('Hi, I am Mila! I am your virtual personal assistant from Asrix Prime. How are you doing today?');
   }
   
   async function defaultFallback(agent) {
@@ -27,7 +27,7 @@ app.post("/dialogflow", express.json(), (req, res) => {
       const dialog = [
         `The following is a conversation with an AI assistant that can have meaningful conversations with users. The assistant is helpful, empathic, and friendly. Its objective is to make the user feel better by feeling heard. With each response, the AI assisstant prompts the user to continue the conversation in a natural way.
 
-        AI: Hello, I am Sophea! I am your virtual personal assistant from Asrix Prime. How are you doing today?`,
+        AI: Hello, I am Mila! I am your virtual personal assistant from Asrix Prime. How are you doing today?`,
       ];
       let query = agent.query;
       console.log('querytext ', query)
