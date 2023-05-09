a2d29543c10270efec9591724dabd9d6935098af
Md Khussairiee Bin Ahmad <111945064+mdkhussairiee@users.noreply.github.com>
2023-05-09 23:25:37 +0800

    Update dialogflowgpt3.js

--- dialogflowgpt3.js
+++ dialogflowgpt3.js
@@ -1,61 +1,56 @@
 const express = require("express");
 require("actions-on-google")
 require('dotenv').config();
-// console.log(process.env)
 const axios = require('axios');
 const { WebhookClient } = require("dialogflow-fulfillment");
 const app = express();
 
 app.post("/dialogflow", express.json(), (req, res) => {
-    const agent = new WebhookClient({ request: req, response: res });
-    let intentMap = new Map();
-    intentMap.set("Default Welcome Intent", welcome);
-    intentMap.set("Default Fallback Intent", defaultFallback);
-    agent.handleRequest(intentMap);
-  
-    function welcome(agent) {
-      agent.add('Hi, I am Eddie! I am your virtual personal assistant from Asrix Prime. How are you doing today?');
-  }
-  
-  async function defaultFallback(agent) {
-      // agent.add('Sorry! I am unable to understand this at the moment. I am still learning humans. You can pick any of the service that might help me.');
-      const instance = axios.create({
-        baseURL: 'https://api.openai.com/v1/',
-        headers: { Authorization: `Bearer ${process.env.OPENAI_API_KEY}` },
-      });
-    
-      const dialog = [
-        `The following is a conversation with an AI assistant that can have meaningful conversations with users. The assistant is helpful, empathic, and friendly. Its objective is to make the user feel better by feeling heard. With each response, the AI assisstant prompts the user to continue the conversation in a natural way.
+  const agent = new WebhookClient({ request: req, response: res });
+  let intentMap = new Map();
+  intentMap.set("Default Welcome Intent", welcome);
+  intentMap.set("Default Fallback Intent", defaultFallback);
+  agent.handleRequest(intentMap);
 
-        AI: Hello, I am Eddie! I am your virtual personal assistant from Asrix Prime. How are you doing today?`,
-      ];
-      let query = agent.query;
-      console.log('querytext ', query)
-      dialog.push(`User: ${query}`);
-      dialog.push('AI:');
-      // agent.add(`you said ${query}`)
-    
-      const completionParmas = {
-        prompt: dialog.join('\n'),
-        max_tokens: 60,
-        temperature: 0.85,
-        n: 1,
-        stream: false,
-        logprobs: null,
-        echo: false,
-        stop: '\n',
-      };
-    
-      try {
-        const result = await instance.post('/engines/text-davinci-003/completions', completionParmas);
-        const botResponse = result.data.choices[0].text.trim();
-        agent.add(botResponse);
-      } catch (err) {
-        console.log(err);
-        agent.add('Sorry. Something went wrong. Can you say that again?');
+  function welcome(agent) {
+    agent.add('Hi, I am Eddie! I am your virtual personal assistant from Asrix Prime. How are you doing today?');
+  }
+
+  async function defaultFallback(agent) {
+    const dialog = [
+      {
+        'role': 'system',
+        'content': 'The following is a conversation with an AI assistant that can have meaningful conversations with users. The assistant is helpful, empathic, and friendly. Its objective is to make the user feel better by feeling heard. With each response, the AI assisstant prompts the user to continue the conversation in a natural way.'
+      },
+      {
+        'role': 'assistant',
+        'content': 'Hello, I am Eddie! I am your virtual personal assistant from Asrix Prime. How are you doing today?'
+      },
+      {
+        'role': 'user',
+        'content': agent.query
       }
-     
+    ];
+
+    const completionParmas = {
+      'model': 'gpt-3.5-turbo',
+      'messages': dialog,
+      'max_tokens': 60,
+      'temperature': 0.85,
+    };
+
+    try {
+      const result = await axios.post('https://api.openai.com/v1/chat/completions', completionParmas, {
+        headers: { 'Authorization': `Bearer ${process.env.OPENAI_API_KEY}` },
+      });
+      const botResponse = result.data.choices[0].message.content.trim();
+      agent.add(botResponse);
+    } catch (err) {
+      console.log(err);
+      agent.add('Sorry. Something went wrong. Can you say that again?');
+    }
   }
 });
+
 const port = 3000;
 app.listen(port, () => console.log(`App listening on port ${port}!`))
